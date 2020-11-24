2085ce7a0e19a3bc68a65c1c5c11076bde3aad86
Bishwo Purkuti <bishwopurkutihhs@gmail.com>
2020-11-23 18:35:34 -0800

    More

--- c++/maxhourglass.cpp
+++ c++/maxhourglass.cpp
@@ -6,9 +6,13 @@ int maxSumHourGlass(vector<vector<int>> arr){
     int max=INT_MIN;
     int currmax=0;
     
+    //i is each row. Since its 6*6 matrix, we can only have hour glass from
+    //1st row to 4th row which is 0-3 hence -2
     for(int i=0; i<arr.size()-2;i++)
     {
-        
+        //j is columns, we can only have 4 hour glass in a row
+        //hence I decided to start the columns from 1-4
+        //we determine the body of an hourglass using these bounds
         for(int j=1; j<arr.size()-1;j++)
         {
             currmax+=arr[i][j-1];
